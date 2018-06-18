b8aa602e82b04ab26b3eb6cfa80ab43df5b40c79
yuto.komasu <yuto.komatsu@rakuten.com>
2018-06-18 01:50:20 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@
 //});
 
 Route::get('/', 'WelcomeController@index');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
