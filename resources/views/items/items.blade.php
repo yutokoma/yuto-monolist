a9f009418136b783bc5fc975e5a687774a8c2254
yuto.komasu <yuto.komatsu@rakuten.com>
2018-06-19 03:42:55 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -20,6 +20,12 @@
                                 @endif
                             </div>
                         </div>
+                         @if (isset($item->count))
+                            <div class="panel-footer">
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Haves</p>
+                            </div>
+                         @endif
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
 
 Route::group(['middleware' => ['auth']], function () {
     //Route::resource('items', 'ItemsController', ['only' => ['create']]);
